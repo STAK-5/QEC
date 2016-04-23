13026b7dcc27d94a759211f2207d65f1562cdd89
Syed M. Taha <s.m.taha10@gmail.com>
2016-04-23 11:35:44 +0500

    Slider Controller added to student

--- public/js/student/controllers.js
+++ public/js/student/controllers.js
@@ -1,3 +1,75 @@
+qecApp.controller('sliderController', ['$scope', '$timeout', 'QueueService', function ($scope, $timeout, QueueService) {
+
+
+    var INTERVAL = 5000,
+        slides = [
+            { id: "image00", src: "imgs/Slider/DUETFromMM.jpg" },
+            //{id: "image00", src: "imgs/Slider/DUETFromMM.jpg"},
+            { id: "image02", src: "imgs/Slider/FlagMarch.jpg" },
+            //{id: "image02", src: "imgs/Slider/FlagMarch.jpg"},
+            { id: "image03", src: "imgs/Slider/Audi.jpg" },
+            //{id: "image03", src: "imgs/Slider/Audi.jpg"},
+            { id: "image03", src: "imgs/Slider/DUET.jpg" },
+            //{id: "image03", src: "imgs/Slider/DUET.jpg"},
+            { id: "image03", src: "imgs/Slider/DUET2.jpg" },
+            //{id: "image03", src: "imgs/Slider/DUET2.jpg"},
+            { id: "image03", src: "imgs/Slider/DUET3.jpg" },
+            //{id: "image03", src: "imgs/Slider/DUET3.jpg"}
+
+        ];
+
+    function setCurrentSlideIndex(index) {
+        $scope.currentIndex = index;
+    }
+
+    function isCurrentSlideIndex(index) {
+        return $scope.currentIndex === index;
+    }
+
+    function nextSlide() {
+        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        $timeout(nextSlide, INTERVAL);
+    }
+
+    function setCurrentAnimation(animation) {
+        $scope.currentAnimation = animation;
+    }
+
+    function isCurrentAnimation(animation) {
+        return $scope.currentAnimation === animation;
+    }
+
+    function loadSlides() {
+        QueueService.loadManifest(slides);
+    }
+
+    $scope.$on('queueProgress', function (event, queueProgress) {
+        $scope.$apply(function () {
+            $scope.progress = queueProgress.progress * 100;
+        });
+    });
+
+    $scope.$on('queueComplete', function (event, slides) {
+        $scope.$apply(function () {
+            $scope.slides = slides;
+            $scope.loaded = true;
+
+            $timeout(nextSlide, INTERVAL);
+        });
+    });
+
+    $scope.progress = 0;
+    $scope.loaded = false;
+    $scope.currentIndex = 0;
+    $scope.currentAnimation = 'slide-left-animation';
+
+    $scope.setCurrentSlideIndex = setCurrentSlideIndex;
+    $scope.isCurrentSlideIndex = isCurrentSlideIndex;
+    $scope.setCurrentAnimation = setCurrentAnimation;
+    $scope.isCurrentAnimation = isCurrentAnimation;
+
+    loadSlides();
+}]);
 
 qecAppStudent.controller('studentLoginController', ['$scope', '$routeParams', '$log', '$http', '$location', '$timeout','$window',
     function ($scope, $routeParams, $log, $http, $location, $timeout,$window) {
@@ -190,5 +262,5 @@ qecAppStudent.controller('questionnaireController', ['$scope', '$routeParams', '
         };
 
 
-    }]);
+}]);
 
